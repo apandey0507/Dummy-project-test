183dfc59db4eb3ae98576c8960bf5d7cfa41c0f6
Arun Pandey <apandey@EX2INDIA.LOCAL>
2023-04-21 14:48:55 +0530

    fix(unit-test): unit test case fixes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Freelancer'`, () => {
+  it(`should have as title 'App Component'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Freelancer');
+    expect(app.title).toEqual('App Component');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Freelancer app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('App Component app is running!');
   });
 });
